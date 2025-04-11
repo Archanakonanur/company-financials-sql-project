8bf8ab7f4356e71ea539d8ed9698e10bffdd55e6
Archana K N <archana.abhishek0115@gmail.com>
2025-04-11 18:42:11 +0530

    Update eda.sql

--- sql_queries/eda.sql
+++ sql_queries/eda.sql
@@ -10,7 +10,7 @@ select distinct product from financials1;
 select distinct year from financials1;
 
 
-# Minimum, maximum & averagw units sold
+# Minimum, maximum & average units sold
 select min(UnitsSold) from financials1 as min_units; 
 select max(UnitsSold) from financials1 as max_units;
 select avg(UnitsSold) from financials1 as avg_units;
