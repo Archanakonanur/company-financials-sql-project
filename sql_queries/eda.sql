1e463257e7b5d3ba73d66861179d041a5c05c2b8
Archana K N <archana.abhishek0115@gmail.com>
2025-04-11 20:18:02 +0530

    Update eda.sql

--- sql_queries/eda.sql
+++ sql_queries/eda.sql
@@ -30,3 +30,44 @@ from financials1
 group by country
 order by total_profit desc;
 ##France had the highest profit of 37,81,020 USD followed by Germany, Canada, USA and Mexico had the least profit of 29,07,523.11 USD.
+
+#Segment wise avg gross margin (Before discount)
+select segment, avg(profit)
+from financials1
+group by segment;
+## It is found that Enterprise has an average loss of 6145 USD, while the rest of the segments all are in profits.
+
+#Segment wise avg net margin (After discount)
+select segment, avg(profit-Discounts) as net_profit
+from financials1
+group by segment;
+
+#Creating a new table to see segment wise  gross profit, net profit and average discount together
+  
+create table profit_margins (segment text, gross_margin int, net_margin int);
+
+insert into profit_margins (segment, gross_margin, net_margin)
+select segment, avg(profit), avg(profit-discounts) from financials1
+group by segment;
+
+alter table profit_margins
+Add avg_discount int;
+
+insert into profit_margins (segment, gross_margin, net_margin, avg_discount)
+select segment, avg(profit), avg(profit-discounts), avg(discounts) from financials1
+group by segment;
+
+
+     ##Deleted the first 10 rows to fix error in code
+       delete from profit_margins
+       where segment is null or avg_discount is null;
+    ## Avg-discount column was not included when the table was created. I altered the table to add the column. But it was showing the values in new rows where the values of other columns were showing null. Hence deleted the first 10 rows
+
+select * from profit_margins;
+## Observed the below:
+  segment       gross_margin   net_margin  avg_discount
+  Enterprise	     -6145	       -20719	     14573
+  Government	      37961	        24965	     12996
+  Midmarket	        6601	        4593	     2008
+  SmallBusiness	    41432	        6294	     35138
+  ChannelPartners	  13168	        11822	      1346
