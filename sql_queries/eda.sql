e84663040840a60732f85e20122a12c3326a7487
Archana K N <archana.abhishek0115@gmail.com>
2025-04-11 18:41:52 +0530

    Create eda.sql

---
+++ sql_queries/eda.sql
@@ -0,0 +1,16 @@
+#Basic EDA
+select * from financials1;
+describe financials1;
+select count(*) from financials1;
+
+#Understanding the distinct values accross segment, country, product and year
+select distinct segment from financials1;
+select distinct country from financials1;
+select distinct product from financials1;
+select distinct year from financials1;
+
+
+# Minimum, maximum & averagw units sold
+select min(UnitsSold) from financials1 as min_units; 
+select max(UnitsSold) from financials1 as max_units;
+select avg(UnitsSold) from financials1 as avg_units;
